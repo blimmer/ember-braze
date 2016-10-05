32cc711d6e5f0d4b3fbab05d5e6a5b8778cd476d
Trevor <trevor@john.tj>
2016-10-05 14:50:49 -0400

    update testing fake api (#26)

--- tests/dummy/snippets/testing.js
+++ tests/dummy/snippets/testing.js
@@ -1,19 +1,53 @@
 // tests/test-helper.js
 
-// Before all tests run, create a mock and replace the standard appboy package.
-let appboyApi = {
-  initialize() {},
-  openSession() {},
-  changeUser() {},
-  logCustomEvent() {},
-  display: {
-    automaticallyShowNewInAppMessages() {},
-    destroyFeed() {},
-    showFeed() {},
-    toggleFeed() {},
-    showInAppMessage() {},
+// Use this as a fake version of the Appboy Api
+const appboyApi = {
+  initialize(apiKey, options) { },
+  changeUser(userId) { },
+  destroy() { },
+  getCachedFeed() { },
+  getUser() { },
+  isPushBlocked() { },
+  isPushGranted(yesCallback, noCallback, safariWebsitePushId) { },
+  isPushSupported() { },
+  logCardClick(card) { },
+  logCardImpressions(cards) { },
+  logCustomEvent(eventName, eventProperties) { },
+  logFeedDisplayed() { },
+  logInAppMessageButtonClick(button, inAppMessage) { },
+  logInAppMessageClick(inAppMessage) { },
+  logInAppMessageImpression(inAppMessage) { },
+  logPurchase(productId, price, currencyCode, quantity, purchaseProperties) { },
+  openSession() { },
+  registerAppboyPushMessages(successCallback, deniedCallback, safariWebsitePushId) { },
+  removeAllSubscriptions() { },
+  removeSubscription(subscriptionGuid) { },
+  requestFeedRefresh() { },
+  requestImmediateDataFlush() { },
+  requestInAppMessageRefresh(count) { },
+  setLogger(loggerFunction) { },
+  submitFeedback(replyToEmail, message, isReportingABug, successCallback, errorCallback) { },
+  subscribeToFeedUpdates(subscriber) { },
+  subscribeToNewInAppMessages(subscriber) { },
+  toggleAppboyLogging() { },
+  unregisterAppboyPushMessages() { },
+  ab: {
+    Feed: { },
+    User: { },
+    InAppMessage: {
+      ClickAction() { }
+    }
   },
+  display: {
+    automaticallyShowNewInAppMessages() { },
+    destroyFeed() { },
+    showFeed() { },
+    toggleFeed() { },
+    showInAppMessage() { }
+  }
 };
+
+// Before all tests run, create a mock and replace the standard appboy package.
 window.appboyMock = sandbox.mock(appboyApi);
 define('appboy', [], () => { return { default: appboyApi }; });
 
