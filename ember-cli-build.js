f641888c1c208e87bf7dce77c772ec9f2f9ce35b
Ben Limmer <ben@ibotta.com>
2017-08-13 11:39:19 -0600

    Upgrade to Ember 2.14.x & Latest Appboy SDK (#44)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
 /* global require, module */
+'use strict';
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     snippetPaths: ['tests/dummy/snippets'],
   });
 
